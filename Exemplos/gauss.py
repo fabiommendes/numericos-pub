9942ec236b9de8eb01d33f4ace1feb13aa32d400
xxxxx <xxxx.@gmail.com>
2017-05-10 12:07:35 -0300

    terminou eliminacao de gauss

--- Exemplos/gauss.py
+++ Exemplos/gauss.py
@@ -13,40 +13,58 @@ import numpy as np
 def solve(matrix, y):
     """
     Resolve um sistema linear dado por uma matriz
-    e um vetor.
+    e um vetor e retorna a solução x correspondente
+    
+        Mx = y,
+    
+    onde M é a matrix fornecida e y é um vetor.
     """
-    M = extended_matrix(matrix, y).astype(float)
-    print('extendida')
-    print(M)
-    upper_triangular(M)
-    print('escalonamento')
-    print(M)
+    
+    # Cria a matriz extendida, convertendo-a para float
+    M =  np.hstack([matrix, y[:, None]]).astype(float)
+    
+    # Escalona a matriz M modificando-a na função
+    escalonate(M)
+    
+    # Normaliza os pivos para 1, modificando a matriz
+    # M na função
     normalize_pivot(M)
-    print('normalizacao')
-    print(M)
+    
+    # Resolve o sistema triangular superior
     return solve_upper(M[:, 0:-1], M[:, -1])
     
-def extended_matrix(matrix, y):
-    """
-    Retorna a matriz extendida pelo vetor v.
-    """
-    return np.hstack([matrix, y[:, None]])
     
-def upper_triangular(M):
+def escalonate(M):
     """
     Modifica a matriz M até atingir a forma triangular
     superior. (escalonamento)
     """
-    # só para matrizes 2 x 2!
-    pivot = M[0, 0]
-    M[1] = M[1] - (M[1, 0] / pivot) * M[0]
+    
+    n = len(M)
+    
+    for k in range(0, n - 1):
+        # Pivoteamento: reordenar as linhas para que o
+        # pivo seja o maior possível em módulo
+        m = np.argmax(abs(M[k:, k])) + k
+        if m != k:
+            M[m], M[k] = M[k].copy(), M[m].copy()            
+        
+        # Escalonamento: realiza combinaçoes lineares
+        # entre as linhas para zerar os termos abaixo
+        # do pivo
+        pivot = M[k, k]
+        for i in range(k + 1, n):
+            M[i] = M[i] - (M[i, k] / pivot) * M[k]
 
 def normalize_pivot(M):
     """
     Normaliza os pivos da matriz
     """
     for i in range(len(M)):
-        M[i] /= M[i, i]
+        norm = M[i, i]
+        if norm == 0:
+            raise ValueError('matriz singular')
+        M[i] /= norm
     
 def solve_upper(M, y):
     """
@@ -58,30 +76,4 @@ def solve_upper(M, y):
         x[i] = y[i] - sum(M[i] * x)
     return x
     
-        
-### testa funcoes
-matrix = np.array([
-    [2, 1, 0], 
-    [0, 0, 2],
-    [3, 1, 0],
-])
-y = np.array([0, 84, 1])
-  
-print(solve(matrix, y))
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+ 
